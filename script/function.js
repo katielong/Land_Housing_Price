b64cc572b64cb1dacad7258e75402f2a4e9311eb
katielong <yl3239@columbia.edu>
2016-08-03 21:36:01 -0400

    fix interpolation problems in scatterplot

--- script/function.js
+++ script/function.js
@@ -25,6 +25,33 @@ function csv2json(csv){
       return json;
 }
 
+// function to transform csv to json
+function csv2json_scatter(csv){ 
+      var nested = d3.nest()
+        .key(function(d){ return d.cityid; })
+        .entries(csv)
+
+      var p=d3.precisionFixed(0.5); f=d3.format("." + p + "f"); 
+
+      var json = nested.map(function(d){
+        var namerecord = {};
+        namerecord.cityid = d.key; 
+        namerecord.city = d.values[0].city;
+        namerecord.province = d.values[0].province;
+        namerecord.city_eng = d.values[0].city_eng;
+        namerecord.region = +d.values[0].abv_prov;
+
+        namerecord.resid_price = d.values.map(function(c){ return {year: c.year, value: f(+c.resid_price/c.CPI*100)}; });
+        namerecord.resid_area= d.values.map(function(c){ return {year: c.year, value: f(+c.resid_area)}; });
+        namerecord.housing_price = d.values.map(function(c){ return {year: c.year, value: f(+c.housing_price/c.CPI*100)}; });
+        namerecord.salary = d.values.map(function(c){ return {year: c.year, value: f(+c.each_salary/c.CPI*100)}; });
+        namerecord.pop = d.values.map(function(c){ return {year: c.year, value: f(+c.nmzrk)}; });
+        return namerecord;
+      });
+      
+      return json;
+}
+
 // animation: choose variable
 function which(dataset, year, variable){
     var sub = d3.map();
--- script/script-scatter.js
+++ script/script-scatter.js
@@ -3,22 +3,25 @@
 // accessor of variable
 function x(d) { return d.resid_area; }
 function y(d) { return d.resid_price; }
-function radius(d) { return d.pop; }
-function color(d) { return d.region;}
 function key(d) { return d.name; }
 
+//control for the size and color of each dot
+function radius(d) { return d.pop; }
+function color(d) { return d.salary;}
+
 var p=d3.precisionFixed(0.5), f=d3.format("." + p + "f");
+var out;
 
-var margin = {top:19.5, right: 0, bottom: 19.5, left: 39.5},
-    w = 580 - margin.right,
-    h = 450 - margin.top - margin.bottom;
+var margin = {top:19.5, right: 13, bottom: 19.5, left: 35},
+    w = 600 - margin.right,
+    h = 500 - margin.top - margin.bottom;
 
-var xScale = d3.scaleLinear().domain([0, 5000]).range([0, w]),
-    yScale = d3.scaleLinear().domain([0,6500]).range([h, 0]),
-    radiusScale = d3.scaleLinear().domain([0, 1800]).range([0, 40]),
-    colorScale = d3.scaleThreshold().domain([0.1,1.1]).range(["blue","orange"]);
+var xScale = d3.scaleSqrt().domain([0, 5000]).range([0, w]),
+    yScale = d3.scaleSqrt().domain([0,6500]).range([h, 0]),
+    radiusScale = d3.scaleLinear().domain([0, 1800]).range([0, 50]),
+    colorScale = d3.scaleThreshold().domain([9889,11380,13470]).range(['#c6dbef','#6baed6','#2171b5','#08306b']);
 
-var xAxis = d3.axisBottom().scale(xScale).ticks(12, d3.format(",d")),
+var xAxis = d3.axisBottom().scale(xScale).ticks(12, d3.format(",d"));
     yAxis = d3.axisLeft().scale(yScale);
 
 var tooltip = d3.select("#content").append("div")
@@ -63,13 +66,72 @@ var label = svg.append("text")
     .attr("y", 60)
     .attr("x", w)
     .text(2007);
+ 
+//Add legend
+var legend_x = d3.scaleLinear().domain([5000, 35000]).range([0,w+margin.right]),
+    arr = [5090, 9889, 11380, 13470, 34060],
+    legend_h = 20;
+
+var legend_axis = d3.axisTop()
+    .scale(legend_x)
+    .tickSize(5) 
+    .tickValues([5000,12500,20000,27500,35000])
+    .tickFormat(function(d, i) {return arr[i];});
+
+var legend = d3.select("#content")
+              .append("svg")
+              .attr("id","legend")
+              .attr("width", w+margin.right+10)
+              .attr("height", legend_h*3.5);
+
+var g = legend.append("g")
+              .attr("class", "key")
+              .attr("transform", "translate(" + margin.left + "," + legend_h + ")");
+
+legend.append("text")
+    .attr("id","legend-text")
+    .attr("transform", "translate(" + margin.left + "," + legend_h*3 + ")")
+    .text("Per capita Income (RMB)");
+
+g.selectAll("rect")
+  .data(colorScale.range().map(function(color){
+          var d = colorScale.invertExtent(color);
+          if (d[0] == null) d[0] = legend_x.domain()[0];
+          return d;}))
+  .enter()
+  .append("rect")
+  .attr("height", legend_h)
+  .attr("x", function(d, i) { return (w+margin.right)/4 * i; })
+  .attr("width", function(d) { return (w+margin.right)/4; })
+  .style("fill", function(d) { return colorScale(d[0]); });
+
+g.call(legend_axis);
+
+  
+// Select to show all cities or just major/non-major cities
+d3.select("#major").on("change", function(){
+  var val = this.value;
+  if (val=="Only Major Cities"){out=1;}
+  else if (val=="Only Non-major Cities"){out=0;}
+  else {out=2;}
+  d3.selectAll(".dots").remove();
+});
+
+
 
 // Click the Button to begin Animation
 function change(){
   d3.selectAll(".dots").remove();
-  
-  d3.csv("data.csv", function(csv) {
-  var data=csv2json(csv);
+  d3.csv("data.csv", function(csv){
+
+  var data=csv2json_scatter(csv);
+
+  if (out==1 | out==0){
+  data = $.grep(data, function(d){
+    return d.region===out;
+  });
+  }
+
   var dot = svg.append("g")
       .attr("class", "dots")
       .selectAll(".dot")
@@ -82,15 +144,22 @@ function change(){
       .sort(order)
       .on("mouseover", function(d) {
             var m = d3.mouse(d3.select("body").node());
+            var currentcol = $(this).css("fill")
+            // tooltip
             tooltip.style("display", null)
                 .style("left", m[0] + 10 + "px")
                 .style("top", m[1] - 10 + "px");
-            $("#tt_county").html(key(d) + "<br>" + 
-              "Mean Residential Land Price: " + y(d) + " RMB"+
-              "<br>" + "New Residential Land Supply: " + x(d)+ " Acre" +
-              "<br>"+ "Urban Population: " + radius(d)*10000 + " PPl");
+            $("#tt_county").html(key(d)+" City");
+
+            // dot opacity
+            $(".dot").css("opacity", "0.3");
+            $(".dot").filter(function(){
+              return $(this).css("fill")==currentcol;
+            }).css("opacity", "1");
+
         })
         .on("mouseout", function() {
+            $(".dot").css("opacity", "1");
             tooltip.style("display", "none");
         });
 
@@ -105,30 +174,31 @@ function change(){
         .on("mouseover", enableInteraction);
 
   svg.transition()
-      .duration(10000)
+      .duration(20000)
       .tween("year", tweenYear)
       .each("end", enableInteraction);
 
-  // Positions the dots based on data.
+
+  // Position the dots based on data
   function position(dot) {
     dot.attr("cx", function(d) { return xScale(x(d)); })
         .attr("cy", function(d) {return yScale(y(d)); })
         .attr("r", function(d) { return radiusScale(radius(d)); });
   }
 
-  // Defines a sort order so that the smallest dots are drawn on top.
+  // Smallest dots are drawn on top
   function order(a, b) {
     return radius(b) - radius(a);
   }
 
-  // After the transition finishes, you can mouseover to change the year.
+  // Mouseover to change the year
   function enableInteraction() {
     var yearScale = d3.scaleLinear()
         .domain([2007, 2012])
         .range([box.x + 10, box.x + box.width - 10])
         .clamp(true);
 
-    // Cancel the current transition, if any.
+    // Cancel current transition
     svg.transition().duration(0);
 
     overlay
@@ -156,8 +226,7 @@ function change(){
       }
 
     function displayYear(year) {
-      var p=d3.precisionFixed(1); f=d3.format("." + p + "f"); 
-        dot.data(interpolateData(f(year)),key).call(position).sort(order);
+        dot.data(interpolateData(year),key).call(position).sort(order);
         label.text(Math.round(year));
       }
 
@@ -166,13 +235,25 @@ function change(){
           return {
             name: d.city_eng,
             region: d.region,
-            salary: f(d.salary[year-2007]/d.CPI[year-2007]*100),
-            resid_price: f(d.resid_price[year-2007]/d.CPI[year-2007]*100),
-            resid_area: f(d.resid_area[year-2007]),
-            housing_price: f(d.housing_price[year-2007]/d.CPI[year-2007]*100),
-            pop: f(d.pop[year-2007])
+            salary: interpolateValues(d.salary,year),
+            resid_price: interpolateValues(d.resid_price,year),
+            resid_area: interpolateValues(d.resid_area,year),
+            housing_price: interpolateValues(d.housing_price,year),
+            pop: interpolateValues(d.pop, year)
           };
         });
       }
-    });
+
+    function interpolateValues(values,year){
+      var bisect = d3.bisector(function(d) { return d.year; });
+      var i = bisect.left(values,year,0,values.length-1), 
+          a = values[i];
+      if (i>0){
+        var b = values[i-1],
+        t = (year - a.year)/(b.year-a.year);
+        return a.value*(1-t)+b.value*t;
+      }
+      return a.value;
+    }
+  });
 }
